918d8f022d40f996fb33c137604f5cac35e469cb
Danjon03 <50589964+Danjon03@users.noreply.github.com>
2024-12-06 16:31:23 -0600

    Add files via upload

---
+++ HAPI SERVER/server.js.js
@@ -0,0 +1,109 @@
+// server.js
+const Hapi = require('@hapi/hapi');
+const MongoClient = require('mongodb').MongoClient;
+
+
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 3000,
+        host: 'localhost',
+        "routes": {
+            "cors": {
+            "origin": ["*"],
+            "headers": ["Accept", "Content-Type"],
+            "additionalHeaders": ["X-Requested-With"]
+        }
+}
+    });
+
+    // MongoDB connection setup
+    
+    const client = new MongoClient('mongodb://localhost:27017');
+    await client.connect();
+    const db = client.db('webProject');
+    const collection = db.collections("Users");
+    const Posts = db.collections("Posts");
+    const Comments = db.collections("Comments");
+
+                                                                                    //Get All Users
+    server.route({
+        method: 'GET',
+        path: '/api/getUsers',
+        handler: async (request, h) => {
+            const items = await db.collection("Users").find({}).toArray();
+            return items;
+        }
+    });
+
+                                                                                    //Get All Posts
+    server.route({
+        method: 'GET',
+        path: '/api/getAllPosts',
+        handler: async (request, h) =>{
+            const items = await db.collection("Posts").find({}).toArray();
+            return items;
+        }
+    });
+
+                                                                                    //CREATE ACCOUNT
+    server.route({
+        method: 'POST',
+        path: '/api/createAccount',
+        handler: async (request, h) => {
+            console.log(request.payload);
+            const newItem = request.payload;
+            const result = await db.collection("Users").insertOne(newItem);
+            return result;
+        }
+    });
+
+                                                                                    //Create POSTS
+    server.route(
+        {
+            method: 'POST',
+            path: '/api/createPost',
+            handler: async (request, h) => {
+                console.log(request.payload);
+                const newItem = request.payload;
+                const result = await db.collection("Posts").insertOne(newItem);
+                return result;
+        }
+    });
+                                                                                    //Create Comment
+    server.route(
+        {
+            method: 'POST',
+            path: '/api/createComment',
+            handler: async (request, h) => {
+                console.log(request.payload);
+                const newItem = request.payload;
+                const result = await db.collection("Comments").insertOne(newItem);
+                return result;
+        }
+    });
+
+                                                                                    //Get All Comments
+    server.route({
+        method: 'GET',
+        path: '/api/getAllComments',
+        handler: async (request, h) =>{
+            const items = await db.collection("Comments").find({}).toArray();
+            return items;
+        }
+
+
+
+
+    });
+
+    await server.start();
+    console.log('Server running on %s', server.info.uri);
+};
+
+process.on('unhandledRejection', (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
